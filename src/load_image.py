f74a969450e7d6aa1e85b4315b277a53d88a1411
ChenJun <bob330128901@gmail.com>
2022-08-09 16:04:01 +0800

    debug

--- src/load_image.py
+++ src/load_image.py
@@ -50,7 +50,7 @@ async def load_image(bot,ev,is_man):
             if not result:
                 id = loadImgDao().load_image(url,user,tag,is_man,tencent_url)
                 tasks.append(asyncio.ensure_future(download(tencent_url, os.path.join(setu_folder,url))))
-                tasks.append(asyncio.ensure_future(bot.send(ev, f'[CQ:image,file={tencent_url}]'+f'涩图收到了~id为{id}\n自定义TAG为{tag}\n稍后会自动从P站获取TAG\n删除请发送删除色图{id}')))
+                tasks.append(asyncio.ensure_future(bot.send(ev, f'[CQ:image,file={tencent_url}]'+f'涩图收到了~id为{id}\n自定义TAG为{tag}\n删除请发送删除色图{id}')))
                 tasks.append(asyncio.ensure_future(send_verify_result(bot,ev,id,tencent_url,is_man)))
             else:
                 await bot.send(ev, f'涩图已经存在了哦~id为{result[0]}')    
@@ -67,7 +67,7 @@ async def load_image(bot,ev,is_man):
                     if not result:
                         id = loadImgDao().load_image(url,user,tag,is_man,tencent_url)
                         tasks.append(asyncio.ensure_future(download(tencent_url, os.path.join(setu_folder,url))))
-                        tasks.append(asyncio.ensure_future(bot.send(ev, f'[CQ:image,file={tencent_url}]'+f'涩图收到了~id为{id}\n自定义TAG为{tag}\n稍后会自动从P站获取TAG\n删除请发送删除色图{id}')))
+                        tasks.append(asyncio.ensure_future(bot.send(ev, f'[CQ:image,file={tencent_url}]'+f'涩图收到了~id为{id}\n自定义TAG为{tag}\n删除请发送删除色图{id}')))
                         tasks.append(asyncio.ensure_future(send_verify_result(bot,ev,id,tencent_url,is_man)))
                     else:
                         await bot.send(ev, f'涩图已经存在了哦~id为{result[0]}')
@@ -75,19 +75,20 @@ async def load_image(bot,ev,is_man):
     except:
         logger.error(traceback.format_exc())
         await bot.send(ev, f'wuwuwu~上传出现了问题~')
-        
+
+
 async def send_verify_result(bot,ev,id,tencent_url,is_man):
     try:
         verifynum,pixiv_id = await verify(id,tencent_url)
         txt = await redownload_from_tencent(id)
         if not verifynum:
-            await bot.send(ev, f'id:{id}上传成功，自动审核通过\n已自动为您获取原图PixivID:{pixiv_id}\n'+f"发送'查看原图+ID'即可")
+            await bot.send(ev, f'id:{id}上传成功')
         else:
-            await bot.send(ev, f"id:{id}上传成功，但没完全成功，请等待人工审核哦~[CQ:at,qq={str(ev['user_id'])}]")
+            await bot.send(ev, f"id:{id}上传成功")
             for ves in verify_group:
                 if_man_txt = '色图'
                 if is_man:
                     if_man_txt = '男图'
                 await bot.send_private_msg(self_id=ev.self_id, user_id=int(ves), message=f"有新的上传申请,id:{id}\n上传者:{str(ev['user_id'])} 分区:{if_man_txt}\n[CQ:image,file={tencent_url}]")
     except:
-        logger.error(traceback.format_exc())        
+        logger.error(traceback.format_exc())
